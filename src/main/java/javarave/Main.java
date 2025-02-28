9d15fd2617bd417fc093175e8b9e1de534d0c3c8
Aleksandr <screemo@mail.ru>
2025-02-28 23:34:53 +0300

    Пушу сканнер в main!

--- src/main/java/javarave/Main.java
+++ src/main/java/javarave/Main.java
@@ -1,11 +1,19 @@
 package javarave;
 
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 //TIP сюда ачивку мне!
         System.out.printf("Hello, GitHub Achievements!");
 
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "! Welcome to GitHub Achievements!");
+    }
 
     }
 }
