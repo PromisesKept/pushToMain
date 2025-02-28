4decae733b14c29f62a8b3e1ddd9236f9cae6f1f
Aleksandr <screemo@mail.ru>
2025-02-28 23:30:53 +0300

    Initial commit

---
+++ src/main/java/javarave/Main.java
@@ -0,0 +1,11 @@
+package javarave;
+
+
+public class Main {
+    public static void main(String[] args) {
+//TIP сюда ачивку мне!
+        System.out.printf("Hello, GitHub Achievements!");
+
+
+    }
+}
